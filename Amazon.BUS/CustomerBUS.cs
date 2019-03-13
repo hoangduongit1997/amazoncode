adc4a46c2edce5878b36d7438d05556211684942
South <42504835+hoangkhang1612@users.noreply.github.com>
2019-03-13 10:49:19 +0700

    Update CustomerBUS.cs

--- Amazon.BUS/CustomerBUS.cs
+++ Amazon.BUS/CustomerBUS.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ namespace Amazon.BUS
     {
         /// <summary>
         /// Customerbus BUS
+	   /// New
         /// </summary>
         CustomerDAL dal;
         public CustomerBUS()
