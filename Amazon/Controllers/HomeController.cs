3ad6058096371ceb4f0c8fe9beb1e1cc73e9071a
Vuong97 <42527475+Vuong97@users.noreply.github.com>
2019-03-15 17:14:59 +0700

    aa

--- Amazon/Controllers/HomeController.cs
+++ Amazon/Controllers/HomeController.cs
@@ -4,20 +4,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-
 namespace Amazon.Controllers
 {
     public class HomeController : Controller
     {
         HttpClient client;      
-        string url = "http://localhost:62993/api/product";
+        string url = "http://localhost:62993/api";
         // GET: Home
+        public HomeController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
         public async Task<ActionResult> Index()
         {
-            HttpResponseMessage responseMessage = await client.GetAsync(url + "/GetAll");
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/product/GetAll");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var responseData = responseMessage.Content.ReadAsStringAsync().Result;
@@ -26,29 +33,48 @@ namespace Amazon.Controllers
                     NullValueHandling = NullValueHandling.Ignore,
                     MissingMemberHandling = MissingMemberHandling.Ignore
                 };
-                var product = JsonConvert.DeserializeObject<List<ProductDTO>>(responseData, settings);
-
-               // return View(product);
-                //var lst = new ProductBUS().ListNewProduct();
-                ViewBag.NewProduct = product;
-                //var topdeal = new ProductBUS().TopDealProduct();
+                var product = JsonConvert.DeserializeObject<List<ProductDTO>>(responseData, settings);             
+                ViewBag.NewProduct = product;            
                 ViewBag.TopDeal = product;
                 //ViewBag.SessionUser = Session["Customer"];
             }
             return View();
+        }        
+        [ChildActionOnly]
+        public async Task<PartialViewResult> MainMenu()
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/Home/Menu");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                var settings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    MissingMemberHandling = MissingMemberHandling.Ignore
+                };
+                var list_menu = JsonConvert.DeserializeObject<List<MenuDTO>>(responseData, settings);
+                return PartialView(list_menu);
+            }
+           
+            return PartialView();
         }
-
-        //[ChildActionOnly]
-        //public PartialViewResult MainMenu()
-        //{
-        //    var menu = new MenuBUS().ListMenu().ToList();
-        //    return PartialView(menu);
-        //}
-        //public ActionResult Slider()
-        //{
-        //    var slider = new SlideBUS().Sliders().ToList();
-        //    return PartialView(slider);
-        //}
+        [ChildActionOnly]
+        public async  Task<PartialViewResult> Slider()
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url + "/Home/Slide");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                var settings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    MissingMemberHandling = MissingMemberHandling.Ignore
+                };
+                var list_slide =JsonConvert.DeserializeObject<List<SlideDTO>>(responseData, settings);
+                return PartialView(list_slide);
+            }
+            return PartialView();
+        }
         //public PartialViewResult Footer()
         //{
         //    var footer = new FooterBUS().ListFooterBUS().ToList();
---
+++ AmazonWebAPI/Services/MenuRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Amazon.BUS;
+using Amazon.DTO;
+
+namespace AmazonWebAPI.Services
+{
+    public class MenuRepository
+    {
+        MenuBUS bus = null;
+        public MenuRepository()
+        {
+            bus = new MenuBUS();
+        }
+        public List<MenuDTO> ListMenu()
+        {
+            return bus.ListMenu();
+        }
+    }
+}
